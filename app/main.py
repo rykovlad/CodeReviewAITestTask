68d8b684d7731515705dea421c745c87a31ccd17
rykovlad <vlad.rikov.2002@gmail.com>
2024-10-25 01:40:09 +0300

    MVP fully work, but need to optimyze code, add comments, implement redis and poetry

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.include_router(router, prefix="/api/v1")
 
 @app.get("/")
 async def read_root():
-    return {"message": "i`m alive"}
+    return {"message": "i`m alive\n go to http://localhost:8000/docs"}
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="localhost", port=8000)
--- app/services/gpt.py
+++ app/services/gpt.py
@@ -4,6 +4,7 @@ from pprint import pprint
 
 from fastapi import HTTPException
 from openai import OpenAI
+from starlette.responses import PlainTextResponse
 
 from app.services.github import fetch_repo_json
 from app.core.config import config
@@ -13,36 +14,36 @@ client = OpenAI(api_key=config.OPENAI_API_KEY)
 async def analyze_code(assignment_description: str, repo_content: dict, candidate_level: str) -> dict:
     prompt = generate_prompt(repo_content, candidate_level, assignment_description)
 
-    import tiktoken
+    # import tiktoken
+    #
+    # def count_tokens(_text: str, model: str = "gpt-4") -> int:
+    #     encoding = tiktoken.encoding_for_model(model)
+    #     tokens = encoding.encode(_text)
+    #     return len(tokens)
+    #
+    # tokens_count = count_tokens(prompt)
+    # print(f"Number of tokens: {tokens_count}")
 
-    def count_tokens(_text: str, model: str = "gpt-4") -> int:
-        encoding = tiktoken.encoding_for_model(model)
-        tokens = encoding.encode(_text)
-        return len(tokens)
+    # try:
+    response = client.chat.completions.create(
+        model="gpt-4-turbo",
+        # model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a code reviewer"},
+            {"role": "user", "content": prompt}
+            # {"role": "user", "content": "print(10)"}
+        ],
+    )
+    pprint(response)
+    answer = response.choices[0].message.content
 
-    tokens_count = count_tokens(prompt)
-    print(f"Number of tokens: {tokens_count}")
+    print(answer)
 
-    a = 3
+    return extract_review_data(answer)
 
-    try:
-        response = client.chat.completions.create(
-            model="gpt-4-turbo",
-            # model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a code reviewer"},
-                {"role": "user", "content": prompt}
-                # {"role": "user", "content": "print(10)"}
-            ],
-        )
-        pprint(response)
-        answer = response.choices[0].message.content
-
-        return extract_review_data(answer)
-
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail="Failed to analyze the code")
+    # except Exception as e:
+    #     print(e)
+    #     raise HTTPException(status_code=500, detail="Failed to analyze the code")
 
 
 
@@ -57,10 +58,32 @@ def generate_prompt(repo_content: dict, candidate_level: str, assignment_descrip
     {assignment_description}            
          Make structured report in JSON format with the following fields:
     - 'issues': A list of specific issues found in the code in format dictionary with "file" and  "description" keys, 
-    also "file" value is str of filename, and "description" value is list of str of issues
+    also "file" value is str of filename, and "description" value is list of str of issues and number of line with that issue
     - 'rating': A rating of the candidate's performance (e.g., 1-5 or a detailed textual description) in forman "n/5"
     - 'conclusion': A final assessment of the code quality, areas for improvement and and compliance with the assignment description
     - 'analyzed_files': List of all files in promt
+    
+    Provide a **strict JSON** response. **Do not** include any additional text or explanations, only return the JSON object.
+
+    The JSON structure should follow this format:
+    ```json
+    {{
+      "issues": [
+        {{
+          "file": "filename.py",
+          "description": [
+            "Issue 1 description",
+            "Issue 2 description"
+          ]
+        }},
+        ...
+      ],
+      "rating": "n/5",
+      "conclusion": "Final assessment of the code quality and areas for improvement",
+      "analyzed_files": ["file1.py", "file2.py", ...]
+    }}
+
+    
     Project:
     
     {files}    
